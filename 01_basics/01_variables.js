8b5ddc953ed70ea6c2902397fb0f55d1be2058c2
abhinavrajsinghdtu <69721545+abhinavrajsinghdtu@users.noreply.github.com>
2023-12-28 08:19:09 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -6,13 +6,18 @@ accountCity ="Delhi"
 
 accountEmail = "efg.com"
 accountPassword = 2233
-accountAddress = "Noida"
+accountCity = "Noida"
+
+
+accountEmail = "def.com"
+accountPassword = 6969
+accountCity = "Gurgaon"
+
 
 /*
 Note: 
 --->always use const and let keyword in JS to declare variable
-
---->avoid using var to avoid issue in block scope & functional scope
+    avoid using var to avoid issue in block scope & functional scope
 
 ---> ; is optional in Js
 
@@ -20,11 +25,13 @@ Note:
 
 ---> for single variable printing, simply use console.log(variableName)
 
----> for multiple variable printing, simply can repeat console.log(variableName) multiple times
-but to save time & space use, console.table([variableName,variableName, .......])
-this will print multiple variables at atime in a table form.
+---> for multiple variable printing, simply can repeat console.log(variableName) multiple times to print all the variables separately
+but to save time & space use, 
+console.table([variableName,variableName, .......])------>this will print multiple variables at a time in a table form.
 
 ---> variables can be declared in Js without let and const as I did above in line 4 for accountCity, but it's not a good practice.
+
+---> to run the entire code in terminal, open terminal(shortcut----ctrl+ ~) then type, node foldername/filename with .js extension, press enter.
 */
 
 
