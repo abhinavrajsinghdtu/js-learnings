ec7164b3c7281555d543ad06bf4594010b671c61
abhinavrajsinghdtu <69721545+abhinavrajsinghdtu@users.noreply.github.com>
2023-12-28 12:58:44 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treats all JS code as a newer version
+
+// alert (3 + 3) // we are using node.js not browser
+
+console.log (3 + 3) 
+
+// console.log (3 +
+//     3)   -----> don't write the code like this, always remember code readability should be high
+
+console.log("Abhinav")
+
+let name = "Abhinav" // string datatype
+let age = 25        //  number datatype
+let isLoggedIn = true // Boolean datatype
+
+
+// number----> 2 to the power 53; is the range for number in memory address
+// bigint---> rarely used in day to day programming. Basically, used for large numbers or large websites like redit, facebook, stock market, etc.
+// string---> " "
+// boolean--> true/false
+// null---> standalone value
+// undefined---> value not defined
+// symbol ----> unique
+
+
+
+//Object
+
+console.log(typeof age)
+
+console.log(typeof null) //typeof null---->object
+console.log(typeof undefined) //typeof undefined----> undefined
