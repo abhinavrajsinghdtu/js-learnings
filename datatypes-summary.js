7c8848eeafe00d325fd749253027f26d18851981
abhinavrajsinghdtu <69721545+abhinavrajsinghdtu@users.noreply.github.com>
2024-01-05 11:53:20 +0000

    datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,39 @@
+//  // Primitive datatypes
+// // 7 types : string, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+
+
+
+// const score = 100
+// const scoreValue = 100.5
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol ('123')
+
+// console.log(id === anotherId) // output false 
+// // Note: id and anotherId are different things and gives different results even if the same value is passed.
+
+// const bigNumber = 33547431232345567n
+
+
+//  //Reference (Non primitive) datatypes
+//  // 3 types : Array, Objects, Functions.
+
+//  const heroes = ["Shahid", "Arjun", "Salman"]
+//  let myObj = {
+//     name: "Abhinav"
+//     age: 25
+//  }
+
+
+// Stack Memory(Primitive datatypes container) & Heap Memory( Non-Primitive datatypes container)
+
+let myYoutubename = "abhinavdotcom"
+
+let anothername = myYoutubename
+
+console.log(anothername)
